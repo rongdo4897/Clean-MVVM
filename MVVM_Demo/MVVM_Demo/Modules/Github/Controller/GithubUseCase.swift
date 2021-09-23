13eba7e3a66181c9e48f9d016048a79b14394e43
Hoang Lam <tunglam977@gmail.com>
2021-09-23 17:08:52 +0700

    first commit

---
+++ MVVM_Demo/MVVM_Demo/Modules/Github/Controller/GithubUseCase.swift
@@ -0,0 +1,28 @@
+//
+//  GithubUseCase.swift
+//  MVVM_Demo
+//
+//  Created by Hoang Lam on 23/09/2021.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class GithubUseCase {
+//    func getDataProfile(completion: @escaping ((ResponseServerEntity<GithubResponse>?) -> Void)) {
+//        ApiManage.shared.request(router: .github) { (data) in
+//            completion(data)
+//        }
+//    }
+    
+    let decoder = JSONDecoder()
+    
+    func getDataProfile(completion: @escaping (GithubResponse?) -> Void) {
+        AF.request("https://api.github.com/search/users?q=rongdo").responseJSON { response in
+            guard let data = response.data, let datas = try? self.decoder.decode(GithubResponse.self, from: data) else {return}
+            
+            completion(datas)
+        }
+    }
+}
